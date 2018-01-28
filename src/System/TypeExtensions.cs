dd9e3a699ca9caa5381915373b80fbe8b6a84e6e
Oleg Sych <olegsych@users.noreply.github.com>
2018-01-27 17:40:41 -0800

    Make TypeExtensions.Constructor<>(Type) methods call Constructor(Type, params) to reduce duplication

--- src/System/TypeExtensions.cs
+++ src/System/TypeExtensions.cs
@@ -8,50 +8,42 @@ namespace System
     {
         public static ConstructorInfo Constructor<T>(this Type type)
         {
-            var inspector = TypeInspector.Create(type);
-            return inspector.GetConstructor(typeof(T));
+            return Constructor(type, typeof(T));
         }
 
         public static ConstructorInfo Constructor<T1, T2>(this Type type)
         {
-            var inspector = TypeInspector.Create(type);
-            return inspector.GetConstructor(typeof(T1), typeof(T2));
+            return Constructor(type, typeof(T1), typeof(T2));
         }
 
         public static ConstructorInfo Constructor<T1, T2, T3>(this Type type)
         {
-            var inspector = TypeInspector.Create(type);
-            return inspector.GetConstructor(typeof(T1), typeof(T2), typeof(T3));
+            return Constructor(type, typeof(T1), typeof(T2), typeof(T3));
         }
 
         public static ConstructorInfo Constructor<T1, T2, T3, T4>(this Type type)
         {
-            var inspector = TypeInspector.Create(type);
-            return inspector.GetConstructor(typeof(T1), typeof(T2), typeof(T3), typeof(T4));
+            return Constructor(type, typeof(T1), typeof(T2), typeof(T3), typeof(T4));
         }
 
         public static ConstructorInfo Constructor<T1, T2, T3, T4, T5>(this Type type)
         {
-            var inspector = TypeInspector.Create(type);
-            return inspector.GetConstructor(typeof(T1), typeof(T2), typeof(T3), typeof(T4), typeof(T5));
+            return Constructor(type, typeof(T1), typeof(T2), typeof(T3), typeof(T4), typeof(T5));
         }
 
         public static ConstructorInfo Constructor<T1, T2, T3, T4, T5, T6>(this Type type)
         {
-            var inspector = TypeInspector.Create(type);
-            return inspector.GetConstructor(typeof(T1), typeof(T2), typeof(T3), typeof(T4), typeof(T5), typeof(T6));
+            return Constructor(type, typeof(T1), typeof(T2), typeof(T3), typeof(T4), typeof(T5), typeof(T6));
         }
 
         public static ConstructorInfo Constructor<T1, T2, T3, T4, T5, T6, T7>(this Type type)
         {
-            var inspector = TypeInspector.Create(type);
-            return inspector.GetConstructor(typeof(T1), typeof(T2), typeof(T3), typeof(T4), typeof(T5), typeof(T6), typeof(T7));
+            return Constructor(type, typeof(T1), typeof(T2), typeof(T3), typeof(T4), typeof(T5), typeof(T6), typeof(T7));
         }
 
         public static ConstructorInfo Constructor<T1, T2, T3, T4, T5, T6, T7, T8>(this Type type)
         {
-            var inspector = TypeInspector.Create(type);
-            return inspector.GetConstructor(typeof(T1), typeof(T2), typeof(T3), typeof(T4), typeof(T5), typeof(T6), typeof(T7), typeof(T8));
+            return Constructor(type, typeof(T1), typeof(T2), typeof(T3), typeof(T4), typeof(T5), typeof(T6), typeof(T7), typeof(T8));
         }
 
         public static ConstructorInfo Constructor(this Type type, params Type[] parameters)
