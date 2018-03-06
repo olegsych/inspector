9f94fbf44d2ec78f5cb37fa56f3784256c55dc24
Oleg Sych <olegsych@users.noreply.github.com>
2018-03-05 20:45:01 -0800

    Add ConstructorAccessScenario

---
+++ scenarios/ConstructorAccessScenario.cs
@@ -0,0 +1,53 @@
+﻿using System.Reflection;
+using Xunit;
+
+namespace Inspector
+{
+    public class ConstructorAccessScenario
+    {
+        public class Invoke : ConstructorAccessScenario
+        {
+            class Foo
+            {
+                public int bar;
+                public Foo(int bar) => this.bar = bar;
+
+                public static int staticBar;
+
+                static Foo() {
+                    staticBar = 0;
+                }
+            }
+
+            [Fact]
+            public void ConstructorCreatesNewInstance() {
+                ConstructorInfo constructor = typeof(Foo).Constructor<int>();
+
+                object foo = constructor.Invoke(new object[] { 42 });
+
+                var typedFoo = Assert.IsType<Foo>(foo);
+                Assert.Equal(42, typedFoo.bar);
+            }
+
+            [Fact]
+            public void ConstructorReinitializesExistingInstance() {
+                ConstructorInfo constructor = typeof(Foo).Constructor<int>();
+                var foo = new Foo(0);
+
+                object result = constructor.Invoke(foo, new object[] { 42 });
+
+                Assert.Null(result);
+                Assert.Equal(42, foo.bar);
+            }
+
+            [Fact]
+            public void StaticConstructorReinitializesType() {
+                Foo.staticBar = 42;
+
+                typeof(Foo).TypeInitializer.Invoke(null, null);
+
+                Assert.Equal(0, Foo.staticBar);
+            }
+        }
+    }
+}
