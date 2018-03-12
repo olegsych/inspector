08045a2eef1f64539b5908870e3ad3806a7823b5
Oleg Sych <olegsych@users.noreply.github.com>
2018-03-11 21:38:52 -0700

    Introduce Constructor class

---
+++ src/Constructor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+
+namespace Inspector
+{
+    public class Constructor : Member<ConstructorInfo>
+    {
+        public object Invoke(params object[] args) =>
+            throw new NotImplementedException();
+    }
+
+    public class Constructor<TSignature> : Constructor
+    {
+        public new TSignature Invoke =>
+            throw new NotImplementedException();
+    }
+}
