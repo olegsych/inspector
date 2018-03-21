331278d3d90dce396508f675ddbb7644a4da88e2
Oleg Sych <olegsych@users.noreply.github.com>
2018-03-20 21:35:43 -0700

    Implement Field class

--- src/Field.cs
+++ src/Field.cs
@@ -8,18 +8,40 @@ namespace Inspector
     /// </summary>
     public class Field : Member<FieldInfo>
     {
-        public Field(FieldInfo info, object instance) : base(null, null) =>
-            throw new NotImplementedException();
-
-        public object Value {
-            get => throw new NotImplementedException();
-            set => throw new NotImplementedException();
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Field"/> class.
+        /// </summary>
+        public Field(FieldInfo info, object instance) : base(info, instance) {
+            if(info.IsStatic) {
+                if(instance != null)
+                    throw new ArgumentException($"Instance shouldn't be specified for static field {info.Name}.", nameof(instance));
+            }
+            else {
+                if(instance == null)
+                    throw new ArgumentNullException(nameof(instance), $"Instance is required for field {info.Name}.");
+                if(!info.DeclaringType.GetTypeInfo().IsAssignableFrom(instance.GetType()))
+                    throw new ArgumentException($"Instance type {instance.GetType().Name} doesn't match type {info.DeclaringType.Name} where {info.Name} is declared.", nameof(instance));
+            }
         }
 
-        public object Get() =>
-            throw new NotImplementedException();
+        /// <summary>
+        /// Gets or sets the field value.
+        /// </summary>
+        public object Value {
+            get => Get();
+            set => Set(value);
+        }
 
+        /// <summary>
+        /// Gets the field value.
+        /// </summary>
+        public object Get() =>
+            Info.GetValue(Instance);
+
+        /// <summary>
+        /// Sets the field value.
+        /// </summary>
         public void Set(object value) =>
-            throw new NotImplementedException();
+            Info.SetValue(Instance, value);
     }
 }
---
+++ tst/FieldTest.cs
@@ -0,0 +1,119 @@
+using System;
+using System.Reflection;
+using Xunit;
+
+namespace Inspector
+{
+    public class FieldTest
+    {
+        readonly Field sut;
+
+        // Constructor parameters
+        readonly FieldInfo instanceField = typeof(TestType).GetField(nameof(TestType.Field));
+        readonly FieldInfo staticField = typeof(TestType).GetField(nameof(TestType.StaticField));
+        readonly object instance = new TestType();
+
+        public FieldTest() =>
+            sut = new Field(instanceField, instance);
+
+        public class Constructor : FieldTest
+        {
+            [Fact]
+            public void InitializesNewInstanceForInstanceField() {
+                Member<FieldInfo> member = sut;
+
+                Assert.Same(instanceField, member.Info);
+                Assert.Same(instance, member.Instance);
+            }
+
+            [Fact]
+            public void InitializesNewInstanceForDerivedInstanceField() {
+                var derived = new DerivedType();
+
+                Member<FieldInfo> member = new Field(instanceField, derived);
+
+                Assert.Same(instanceField, member.Info);
+                Assert.Same(derived, member.Instance);
+            }
+
+            [Fact]
+            public void InitializesNewInstanceForStaticField() {
+                Member<FieldInfo> member = new Field(staticField, null);
+
+                Assert.Same(staticField, member.Info);
+                Assert.Null(member.Instance);
+            }
+
+            [Fact]
+            public void ThrowsDescriptiveExceptionWhenGivenNoInstanceForInstanceField() {
+                var thrown = Assert.Throws<ArgumentNullException>(() => new Field(instanceField, null));
+                Assert.Equal("instance", thrown.ParamName);
+                Assert.StartsWith($"Instance is required for field {instanceField.Name}", thrown.Message);
+            }
+
+            [Fact]
+            public void ThrowsDescriptiveExceptionWhenGivenInstanceForStaticField() {
+                var thrown = Assert.Throws<ArgumentException>(() => new Field(staticField, instance));
+                Assert.Equal("instance", thrown.ParamName);
+                Assert.StartsWith($"Instance shouldn't be specified for static field {staticField.Name}.", thrown.Message);
+            }
+
+            [Fact]
+            public void ThrowsDescriptiveExceptionWhenGivenInstanceOfTypeDifferentFromFieldInfo() {
+                var thrown = Assert.Throws<ArgumentException>(() => new Field(instanceField, new AnotherType()));
+                Assert.Equal("instance", thrown.ParamName);
+                Assert.StartsWith($"Instance type {nameof(AnotherType)} doesn't match type {instanceField.DeclaringType.Name} where {instanceField.Name} is declared.", thrown.Message);
+            }
+        }
+
+        public class Value : FieldTest
+        {
+            [Fact]
+            public void GetsFieldValue() {
+                object value = sut.Value;
+                Assert.Same(((TestType)instance).Field, value);
+            }
+
+            [Fact]
+            public void SetsFieldValue() {
+                object value = new FieldType();
+                sut.Value = value;
+                Assert.Same(value, ((TestType)instance).Field);
+            }
+        }
+
+        public class Get : FieldTest
+        {
+            [Fact]
+            public void GetsFieldValue() {
+                object value = sut.Get();
+                Assert.Same(((TestType)instance).Field, value);
+            }
+        }
+
+        public class Set : FieldTest
+        {
+            [Fact]
+            public void SetsFieldValue() {
+                object value = new FieldType();
+                sut.Set(value);
+                Assert.Same(value, ((TestType)instance).Field);
+            }
+        }
+
+        class TestType
+        {
+            public FieldType Field = new FieldType();
+            public static FieldType StaticField = new FieldType();
+        }
+
+        class AnotherType
+        {
+            public FieldType Field = new FieldType();
+        }
+
+        class DerivedType : TestType { }
+
+        class FieldType { }
+    }
+}
