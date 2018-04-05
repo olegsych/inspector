4d5c5f212308d0ad778b8f64e15ed901e7405aca
Oleg Sych <olegsych@users.noreply.github.com>
2018-04-04 20:13:44 -0700

    Implement Method class

--- src/Method.cs
+++ src/Method.cs
@@ -8,9 +8,24 @@ namespace Inspector
     /// </summary>
     public class Method : Member<MethodInfo>
     {
-        protected Method(MethodInfo info, object instance) : base(info, instance) =>
-            throw new NotImplementedException();
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Method"/> class.
+        /// </summary>
+        public Method(MethodInfo info, object instance = null) : base(info, instance) {
+            if (info.IsStatic) {
+                if(instance != null)
+                    throw new ArgumentException($"Instance shouldn't be specified for static method {info.Name}.", nameof(instance));
+            }
+            else {
+                if(instance == null)
+                    throw new ArgumentNullException(nameof(instance), $"Instance is required for method {info.Name}.");
+            }
+        }
 
-        public object Invoke(params object[] arga) => throw new NotImplementedException();
+        /// <summary>
+        /// Invokes the method with specified <paramref name="parameters"/> and returns the value it returns.
+        /// </summary>
+        public object Invoke(params object[] parameters) =>
+            Info.Invoke(Instance, parameters);
     }
 }
---
+++ tst/MethodTest.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Reflection;
+using NSubstitute;
+using Xunit;
+
+namespace Inspector
+{
+    public class MethodTest
+    {
+        readonly Method sut;
+
+        // Constructor parameters
+        readonly MethodInfo instanceMethod = typeof(TestType).GetMethod(nameof(TestType.Method));
+        readonly MethodInfo staticMethod = typeof(TestType).GetMethod(nameof(TestType.StaticMethod));
+        readonly object instance = Substitute.For<TestType>();
+
+        public MethodTest() =>
+            sut = new Method(instanceMethod, instance);
+
+        public class Constructor : MethodTest
+        {
+            [Fact]
+            public void InitializesNewInstanceForInstanceMethod() {
+                Member<MethodInfo> member = sut;
+
+                Assert.Same(instanceMethod, member.Info);
+                Assert.Same(instance, member.Instance);
+            }
+
+            [Fact]
+            public void InitializesNewInstanceForStaticMethod() {
+                Member<MethodInfo> member = new Method(staticMethod);
+
+                Assert.Same(staticMethod, member.Info);
+                Assert.Null(member.Instance);
+            }
+
+            [Fact]
+            public void ThrowsDescriptiveExceptionWhenGivenNoInstanceForInstanceMethod() {
+                var thrown = Assert.Throws<ArgumentNullException>(() => new Method(instanceMethod));
+                Assert.Equal("instance", thrown.ParamName);
+                Assert.StartsWith($"Instance is required for method {instanceMethod.Name}", thrown.Message);
+            }
+
+            [Fact]
+            public void ThrowsDescriptiveExceptionWhenGivenInstanceForStaticMEthod() {
+                var thrown = Assert.Throws<ArgumentException>(() => new Method(staticMethod, instance));
+                Assert.Equal("instance", thrown.ParamName);
+                Assert.StartsWith($"Instance shouldn't be specified for static method {staticMethod.Name}.", thrown.Message);
+            }
+        }
+
+        public class Invoke : MethodTest
+        {
+            [Fact]
+            public void CallsMethodAndReturnsItsResult() {
+                var expectedParameter = new ParameterType();
+                var expectedResult = new ReturnType();
+                ((TestType)instance).Method(expectedParameter).Returns(expectedResult);
+
+                object actualResult = sut.Invoke(expectedParameter);
+
+                Assert.Same(expectedResult, actualResult);
+            }
+        }
+
+        public class TestType
+        {
+            public virtual ReturnType Method(ParameterType p) => null;
+            public static ReturnType StaticMethod(ParameterType p) => null;
+        }
+
+        public class ParameterType { }
+
+        public class ReturnType { }
+    }
+}
