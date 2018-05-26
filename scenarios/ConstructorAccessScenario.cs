56e9c1db3eb7d7485b48571cb6d1801d3b18bd57
Oleg Sych <olegsych@users.noreply.github.com>
2018-05-26 14:35:13 -0700

    Work in progress - ParameterInfoMatcher

--- scenarios/ConstructorAccessScenario.cs
+++ scenarios/ConstructorAccessScenario.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Reflection;
+using System.Runtime.Serialization;
 using Xunit;
 
 namespace Inspector
@@ -51,6 +53,75 @@ namespace Inspector
                 constructor.Invoke(null, null);
 
                 Assert.Equal(0, Foo.staticBar);
+            }
+
+            [Fact]
+            public void ConstructorDelegate() {
+                ConstructorInfo actionInfo = typeof(Action<Foo, int>).GetConstructor(new Type[] { typeof(object), typeof(IntPtr) });
+                Assert.NotNull(actionInfo);
+
+                ConstructorInfo fooInfo = typeof(Foo).GetConstructor(new Type[] { typeof(int) });
+                Assert.NotNull(fooInfo);
+
+                var ctor = (Action<Foo, int>)actionInfo.Invoke(new object[] { null, fooInfo.MethodHandle.GetFunctionPointer() });
+                Assert.NotNull(ctor);
+
+                var foo = (Foo)FormatterServices.GetUninitializedObject(typeof(Foo));
+                ctor.Invoke(foo, 42);
+
+                Assert.Equal(42, foo.bar);
+            }
+
+            [Fact]
+            public void ConstructorDelegate2() {
+                ConstructorInfo actionInfo = typeof(Action<int>).GetConstructor(new Type[] { typeof(object), typeof(IntPtr) });
+                Assert.NotNull(actionInfo);
+
+                ConstructorInfo fooInfo = typeof(Foo).GetConstructor(new Type[] { typeof(int) });
+                Assert.NotNull(fooInfo);
+
+                var foo = (Foo)FormatterServices.GetUninitializedObject(typeof(Foo));
+
+                var ctor = (Action<int>)actionInfo.Invoke(new object[] { foo, fooInfo.MethodHandle.GetFunctionPointer() });
+                Assert.NotNull(ctor);
+
+                ctor.Invoke(42);
+
+                Assert.Equal(42, foo.bar);
+            }
+
+            [Fact]
+            public void Signature() {
+                var signatureType = Type.GetType("System.Signature");
+                Assert.NotNull(signatureType);
+
+                var iRuntimeMethodInfoType = Type.GetType("System.IRuntimeMethodInfo");
+                Assert.NotNull(iRuntimeMethodInfoType);
+
+                var runtimeTypeType = Type.GetType("System.RuntimeType");
+                Assert.NotNull(runtimeTypeType);
+
+                ConstructorInfo signatureCtor = signatureType.GetConstructor(new[] { iRuntimeMethodInfoType, runtimeTypeType });
+                Assert.NotNull(signatureCtor);
+
+                ConstructorInfo fooCtor = typeof(Foo).GetConstructor(new[] { typeof(int) });
+                Assert.NotNull(fooCtor);
+
+                object fooCtorSignature = signatureCtor.Invoke(new object[] { fooCtor, typeof(Foo) });
+                Assert.NotNull(fooCtorSignature);
+
+                MethodInfo invokeMethod = typeof(Action<int>).GetMethod("Invoke");
+                Assert.NotNull(invokeMethod);
+
+                object invokeMethodSignature = signatureCtor.Invoke(new object[] { invokeMethod, typeof(Action<int>) });
+                Assert.NotNull(invokeMethodSignature);
+
+                MethodInfo compareSigMethod = signatureType.GetMethod("CompareSig", BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+                Assert.NotNull(compareSigMethod);
+
+                object result = compareSigMethod.Invoke(null, new object[] { fooCtorSignature, invokeMethodSignature });
+                bool equal = Assert.IsType<bool>(result);
+                Assert.True(equal);
             }
         }
     }
---
+++ tst/ParameterInfoMatcher.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Reflection;
+
+namespace Inspector
+{
+    static class ParameterInfoMatcher
+    {
+        internal static Func<ParameterInfo, ParameterInfo, bool> Match = (p1, p2) => {
+            if (p1 == null)
+                throw new ArgumentNullException(nameof(p1));
+            if(p2 == null)
+                throw new ArgumentNullException(nameof(p2));
+
+            return p1.ParameterType == p2.ParameterType
+                && p1.IsIn == p2.IsIn;
+        };
+    }
+}
