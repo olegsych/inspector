36577101dbd9ad74df6db67980e0154edfc75a5a
Oleg Sych <olegsych@users.noreply.github.com>
2018-07-07 13:21:55 -0700

    Implement MethodDelegateFactory

---
+++ src/MethodDelegateFactory.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Reflection;
+
+namespace Inspector
+{
+    sealed class MethodDelegateFactory : IDelegateFactory<MethodInfo>
+    {
+        bool IDelegateFactory<MethodInfo>.TryCreate(Type delegateType, object target, MethodInfo method, out Delegate @delegate) {
+            if(delegateType == null)
+                throw new ArgumentNullException(nameof(delegateType)); // use descriptive ParamName
+            @delegate = Delegate.CreateDelegate(delegateType, target, method, false);
+            return @delegate != null;
+        }
+    }
+}
