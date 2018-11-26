4371414fb872870baafafe7d1a37b2a28c6e2447
Oleg Sych <olegsych@users.noreply.github.com>
2018-11-25 18:12:42 -0800

    Implement Property<T>

--- src/Property.Generic.cs
+++ src/Property.Generic.cs
@@ -1,29 +1,45 @@
 using System;
-using System.Reflection;
 
 namespace Inspector
 {
     /// <summary>
     /// Provides access to a property of type <typeparamref name="T"/>.
     /// </summary>
-    /// <typeparam name="T">Property type</typeparam>
+    /// <typeparam name="T">Type of property value</typeparam>
     public class Property<T> : Property
     {
-        public Property(PropertyInfo info, object instance) : base(null, null) =>
-            throw new NotImplementedException();
-
-        public new T Value {
-            get => throw new NotImplementedException();
-            set => throw new NotImplementedException();
+        internal Property(Property property) : base(NotNull(property).Info, property.Instance) {
+            if(typeof(T) != Info.PropertyType)
+                throw new ArgumentException($"Property type {Info.PropertyType.FullName} doesn't match expected {typeof(T).FullName}.", nameof(property));
         }
 
+        /// <summary>
+        /// Gets or sets the property value.
+        /// </summary>
+        public new T Value {
+            get => Get();
+            set => Set(value);
+        }
+
+        /// <summary>
+        /// Gets the property value.
+        /// </summary>
         public new T Get() =>
-            throw new NotImplementedException();
+            (T)base.Get();
 
+        /// <summary>
+        /// Sets the property value.
+        /// </summary>
         public void Set(T value) =>
-            throw new NotImplementedException();
+            base.Set(value);
 
-        public static implicit operator T(Property<T> field) =>
-            throw new NotImplementedException();
+        /// <summary>
+        /// Implicitly converts the property to it's value for convenient use in assertions.
+        /// </summary>
+        public static implicit operator T(Property<T> property) =>
+            property != null ? property.Get() : default;
+
+        static Property NotNull(Property property) =>
+            property ?? throw new ArgumentNullException(nameof(property));
     }
 }
---
+++ tst/PropertyTest.Generic.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Linq;
+using System.Reflection;
+using Xunit;
+
+namespace Inspector
+{
+    public class GenericPropertyTest
+    {
+        readonly Property<PropertyType> sut;
+
+        // Constructor parameters
+        readonly Property property;
+
+        // Test fixture
+        readonly InstanceType instance = new InstanceType();
+
+        public GenericPropertyTest() {
+            PropertyInfo info = typeof(InstanceType).GetProperties().Single(_ => _.PropertyType == typeof(PropertyType));
+            property = new Property(info, instance);
+
+            sut = new Property<PropertyType>(property);
+        }
+
+        public class Constructor : GenericPropertyTest
+        {
+            [Fact]
+            public void InitializesBaseWithGivenArgument() {
+                Property @base = sut;
+                Assert.Same(property.Info, @base.Info);
+                Assert.Same(property.Instance, @base.Instance);
+            }
+
+            [Fact]
+            public void ThrowsDescriptiveExceptionWhenPropertyIsNull() {
+                var thrown = Assert.Throws<ArgumentNullException>(() => new Property<PropertyType>(null));
+                Assert.Equal("property", thrown.ParamName);
+            }
+
+            [Fact]
+            public void ThrowsDescriptiveExceptionWhenInfoDoesNotHaveExpectedPropertyType() {
+                PropertyInfo unexpected = typeof(InstanceType).GetProperties().Single(_ => _.PropertyType != typeof(PropertyType));
+                var thrown = Assert.Throws<ArgumentException>(() => new Property<PropertyType>(new Property(unexpected, new InstanceType())));
+                Assert.Equal("property", thrown.ParamName);
+                Assert.StartsWith($"Property type {unexpected.PropertyType} doesn't match expected {typeof(PropertyType)}.", thrown.Message);
+            }
+        }
+
+        public class Get : GenericPropertyTest
+        {
+            [Fact]
+            public void GetsPropertyValue() {
+                PropertyType value = sut.Get();
+                Assert.Same(instance.Property, value);
+            }
+        }
+
+        public class Set : GenericPropertyTest
+        {
+            [Fact]
+            public void SetsPropertyValue() {
+                var value = new PropertyType();
+                sut.Set(value);
+                Assert.Same(value, instance.Property);
+            }
+        }
+
+        public class Value : GenericPropertyTest
+        {
+            [Fact]
+            public void GetsPropertyValue() {
+                PropertyType value = sut.Value;
+                Assert.Same(instance.Property, value);
+            }
+
+            [Fact]
+            public void SetsPropertyValue() {
+                var value = new PropertyType();
+                sut.Value = value;
+                Assert.Same(value, instance.Property);
+            }
+        }
+
+        public class ImplicitOperatorT : GenericPropertyTest
+        {
+            [Fact]
+            public void ImplicitlyConvertsPropertyToValueType() {
+                PropertyType value = sut;
+                Assert.Same(value, instance.Property);
+            }
+
+            [Fact]
+            public void ConvertsNullPropertyToDefaultValueToSupportImplicitTypeConversionRules() {
+                PropertyType value = (Property<PropertyType>)null;
+                Assert.Null(value);
+            }
+        }
+
+        class InstanceType
+        {
+            public PropertyType Property { get; set; } = new PropertyType();
+            public string AnotherProperty { get; set; } = string.Empty;
+        }
+
+        class PropertyType { }
+    }
+}
