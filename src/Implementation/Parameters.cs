2e67bf90518fd64fbea1b5ea8a301e0ff5c95d1e
Oleg Sych <olegsych@users.noreply.github.com>
2019-09-02 12:58:00 -0700

    Implementation/Parameters.cs: First implementation

---
+++ src/Implementation/Parameters.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Inspector.Implementation
+{
+    sealed class Parameters: IFilter<ParameterInfo>, IEnumerable<ParameterInfo>
+    {
+        public Parameters(MethodBase method) =>
+            Method = method ?? throw new ArgumentNullException(nameof(method));
+
+        public MethodBase Method { get; }
+
+        public IEnumerator<ParameterInfo> GetEnumerator() {
+            IEnumerable<ParameterInfo> parameters = Method.GetParameters();
+            return parameters.GetEnumerator();
+        }
+
+        IEnumerable<ParameterInfo> IFilter<ParameterInfo>.Get() =>
+            this;
+
+        IEnumerator IEnumerable.GetEnumerator() =>
+            GetEnumerator();
+    }
+}
---
+++ tst/Implementation/ParametersTest.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Xunit;
+using static Inspector.Substitutes;
+
+namespace Inspector.Implementation
+{
+    public class ParametersTest
+    {
+        readonly IEnumerable<ParameterInfo> sut;
+
+        readonly ParameterInfo[] parameters = new[] { ParameterInfo(), ParameterInfo(), ParameterInfo() };
+        readonly MethodBase method;
+
+        public ParametersTest() {
+            method = MethodBase(parameters);
+            sut = new Parameters(method);
+        }
+
+        public class Constructor: ParametersTest
+        {
+            [Fact]
+            public void ThrowsDescriptiveExceptionWhenMethodIsNull() {
+                var thrown = Assert.Throws<ArgumentNullException>(() => new Parameters(null));
+                Assert.Equal("method", thrown.ParamName);
+            }
+        }
+
+        public class Enumerator: ParametersTest
+        {
+            [Fact]
+            public void ReturnsParametersOfMethodGivenToConstructor() =>
+                Assert.Equal(parameters, sut.ToArray()); // Force IEnumerable<ParameterInfo>.GetEnumerator call
+        }
+
+        public class UntypedEnumerator: ParametersTest
+        {
+            [Fact]
+            public void ReturnsParametersOfMethodGivenToConstructor() =>
+                Assert.Equal(parameters, (IEnumerable)sut); // Force IEnumerable.GetEnumerator call
+        }
+
+        public class Get: ParametersTest
+        {
+            new readonly IFilter<ParameterInfo> sut;
+
+            public Get() =>
+                sut = new Parameters(method);
+
+            [Fact]
+            public void ReturnsItself() =>
+                Assert.Same(sut, sut.Get());
+        }
+
+        public class Method: ParametersTest
+        {
+            [Fact]
+            public void ReturnsMethodGivenToConstructor() =>
+                Assert.Same(method, ((Parameters)sut).Method);
+        }
+    }
+}
