d04f54e7ab3677f58f59fd54edcff900de085779
Oleg Sych <olegsych@users.noreply.github.com>
2021-01-30 13:15:43 -0800

    Make TypeAccessExample use nullable annotations

--- examples/TypeAccessExample.cs
+++ examples/TypeAccessExample.cs
@@ -10,16 +10,16 @@ namespace Inspector
         static class Foo
         {
 #pragma warning disable 414
-            public static Bar barField;
+            public static Bar? barField;
 #pragma warning restore 414
 
-            public static Baz BazProperty { get; set; }
+            public static Baz? BazProperty { get; set; }
 
 #pragma warning disable 67
-            public static event EventHandler<Bar> BarEvent;
+            public static event EventHandler<Bar>? BarEvent;
 #pragma warning restore 67
 
-            public static Baz BarFunc(Bar _) => default;
+            public static Baz? BarFunc(Bar _) => default;
 
             public static void BarAction(Bar _) { }
 
