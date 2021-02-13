4f4b6fedeceafab170baeb1795297507f0ae9db7
Oleg Sych <olegsych@users.noreply.github.com>
2021-02-13 11:31:37 -0800

    Make IDelegateFactory, extensions and implementations use nullable target and delegate parameters

--- src/Implementation/IDelegateFactoryExtensions.cs
+++ src/Implementation/IDelegateFactoryExtensions.cs
@@ -6,15 +6,15 @@ namespace Inspector.Implementation
     // Tested through Method<T> and Constructor<T>
     static class IDelegateFactoryExtensions
     {
-        internal static bool TryCreate<TMethodBase, TDelegate>(this IDelegateFactory<TMethodBase> delegateFactory, object target, TMethodBase method, out TDelegate @delegate)
+        internal static bool TryCreate<TMethodBase, TDelegate>(this IDelegateFactory<TMethodBase> delegateFactory, object? target, TMethodBase method, out TDelegate? @delegate)
             where TMethodBase : MethodBase
             where TDelegate : Delegate
         {
             if(delegateFactory == null)
                 throw new ArgumentNullException(nameof(delegateFactory));
 
-            bool created = delegateFactory.TryCreate(typeof(TDelegate), target, method, out Delegate d);
-            @delegate = (TDelegate)d;
+            bool created = delegateFactory.TryCreate(typeof(TDelegate), target, method, out Delegate? d);
+            @delegate = (TDelegate?)d;
             return created;
         }
     }
