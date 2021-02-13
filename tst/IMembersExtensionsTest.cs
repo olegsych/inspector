4b7f17eab012fc19579ec5781e4f20ebd68ac0d7
Oleg Sych <olegsych@users.noreply.github.com>
2021-02-13 13:48:16 -0800

    Cleanup IMembersExtensionsTest.cs

--- tst/IMembersExtensionsTest.cs
+++ tst/IMembersExtensionsTest.cs
@@ -177,7 +177,7 @@ namespace Inspector
         public void InternalReturnsInternalMembers() {
             IMembers actual = members.Internal();
 
-            var accessibleMembers = Assert.IsType<AccessibleMembers>(actual);
+            var accessibleMembers = (AccessibleMembers)actual;
             Assert.Equal(Accessibility.Internal, accessibleMembers.Accessibility);
         }
 
@@ -256,7 +256,7 @@ namespace Inspector
         public void PrivateReturnsPrivateMembers() {
             IMembers actual = members.Private();
 
-            var accessibleMembers = Assert.IsType<AccessibleMembers>(actual);
+            var accessibleMembers = (AccessibleMembers)actual;
             Assert.Equal(Accessibility.Private, accessibleMembers.Accessibility);
         }
 
@@ -320,7 +320,7 @@ namespace Inspector
         public void ProtectedReturnsProtectedMembers() {
             IMembers actual = members.Protected();
 
-            var accessibleMembers = Assert.IsType<AccessibleMembers>(actual);
+            var accessibleMembers = (AccessibleMembers)actual;
             Assert.Equal(Accessibility.Protected, accessibleMembers.Accessibility);
         }
 
@@ -328,18 +328,18 @@ namespace Inspector
         public void PublicReturnsPublicMembers() {
             IMembers actual = members.Public();
 
-            var accessibleMembers = Assert.IsType<AccessibleMembers>(actual);
+            var accessibleMembers = (AccessibleMembers)actual;
             Assert.Equal(Accessibility.Public, accessibleMembers.Accessibility);
         }
 
         static void VerifyDeclaredMembers<TDeclaringType>(IMembers source, IMembers actual) {
-            var declaredMembers = Assert.IsType<DeclaredMembers>(actual);
+            var declaredMembers = (DeclaredMembers)actual;
             Assert.Equal(typeof(TDeclaringType), declaredMembers.DeclaringType);
             Assert.Same(source, declaredMembers.Source);
         }
 
         static void VerifyInheritedMembers<TAncestorType>(IMembers source, IMembers actual) {
-            var inheritedMembers = Assert.IsType<InheritedMembers>(actual);
+            var inheritedMembers = (InheritedMembers)actual;
             Assert.Equal(typeof(TAncestorType), inheritedMembers.AncestorType);
             Assert.Same(source, inheritedMembers.Source);
         }
