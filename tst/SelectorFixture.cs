e23cab0a5ba0540612337f27ceb47bf107fc3f3a
Oleg Sych <olegsych@users.noreply.github.com>
2021-02-14 10:49:28 -0800

    Cleanup SelectorFixture

--- tst/SelectorFixture.cs
+++ tst/SelectorFixture.cs
@@ -3,19 +3,18 @@ using System.Collections.Generic;
 using System.Reflection;
 using Inspector.Implementation;
 using NSubstitute;
-using Xunit;
 
 namespace Inspector
 {
     public class SelectorFixture<T>: IDisposable
     {
-        readonly FieldInfo field = typeof(Selector<T>).GetField(nameof(Selector<T>.Select), BindingFlags.NonPublic | BindingFlags.Static);
+        readonly FieldInfo field = typeof(Selector<T>).GetField(nameof(Selector<T>.Select), BindingFlags.NonPublic | BindingFlags.Static)!;
         readonly Func<IEnumerable<T>, T> original = Selector<T>.Select;
 
         internal readonly Func<IEnumerable<T>, T> select = Substitute.For<Func<IEnumerable<T>, T>>();
 
         public SelectorFixture() {
-            original = (Func<IEnumerable<T>, T>)field.GetValue(null);
+            original = (Func<IEnumerable<T>, T>)field.GetValue(null!)!;
             field.SetValue(null, select);
         }
 
