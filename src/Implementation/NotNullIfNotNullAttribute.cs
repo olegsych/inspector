82feea89937bb17a32a49c0ccf04e35a78a6ad58
Oleg Sych <olegsych@users.noreply.github.com>
2026-06-14 11:36:49 -0700

    squash! src/Implementation/NotNullIfNotNullAttribute.cs
    Inherited = false to match the real type

--- src/Implementation/NotNullIfNotNullAttribute.cs
+++ src/Implementation/NotNullIfNotNullAttribute.cs
@@ -1,9 +1,11 @@
+// Polyfill type not available on netstandard2.0
+// https://learn.microsoft.com/dotnet/api/system.diagnostics.codeanalysis.notnullifnotnullattribute
+
 #pragma warning disable IDE0130 // Namespace must match the polyfilled type
 namespace System.Diagnostics.CodeAnalysis;
 #pragma warning restore IDE0130
 
-// Polyfill https://learn.microsoft.com/dotnet/api/system.diagnostics.codeanalysis.notnullifnotnullattribute
-[AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.ReturnValue, AllowMultiple = true)]
+[AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.ReturnValue, AllowMultiple = true, Inherited = false)]
 sealed class NotNullIfNotNullAttribute(string parameterName): Attribute
 {
     internal string ParameterName { get; } = parameterName;
